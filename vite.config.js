6297e9fe9faa5cc28d99b72c2e31788bb6717772
palajambu <captpatrickstar@gmail.com>
2024-09-08 23:33:11 +0200

    10 commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/palajambu.github.io/",
+  base: "/",
   plugins: [
     react(),
     // Custom plugin to load markdown files
